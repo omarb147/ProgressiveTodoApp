7bd60eb7a241241fbe22a23cc0390611e4176d12
obello147 <o_bello@live.co.uk>
2019-10-22 11:10:24 +0100

    Updates

---
+++ precache-manifest.d71f52384f2d7de152e06ac27d68b676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d283aa5583ab2391e53f9073227eda",
+    "url": "/ProgressiveTodoApp/index.html"
+  },
+  {
+    "revision": "80cb922bb09dd8ae7044",
+    "url": "/ProgressiveTodoApp/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "3ade10d2a66269362df6",
+    "url": "/ProgressiveTodoApp/static/css/main.2aa4f4e4.chunk.css"
+  },
+  {
+    "revision": "80cb922bb09dd8ae7044",
+    "url": "/ProgressiveTodoApp/static/js/2.bb7e904b.chunk.js"
+  },
+  {
+    "revision": "3ade10d2a66269362df6",
+    "url": "/ProgressiveTodoApp/static/js/main.2908d01d.chunk.js"
+  },
+  {
+    "revision": "e5122d2abe38f4f33ea1",
+    "url": "/ProgressiveTodoApp/static/js/runtime-main.df7ae83f.js"
+  }
+]);
